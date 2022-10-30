0e82e475aea17133ee96a460bb4150654c21151a
Hridyansh Singh <100861068+SinghHridyansh@users.noreply.github.com>
2022-10-30 23:23:57 +0530

    Add files via upload

---
+++ 4-Advance-JS/402-DOM-Manipulation/AdvanceJS-402-02/script.js
@@ -0,0 +1,8 @@
+function showimg(){
+    let c=document.getElementById('image');
+    c.style.display="none"
+}
+function hideimg(){
+    let c=document.getElementById('image');
+    c.style.display="block"
+}
